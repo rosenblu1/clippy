4212db2868ed144f4f241fb77f7af207be18a885
rosenblu1 <yaplore@gmail.com>
2023-05-16 17:24:14 -0400

    more workflow experimentation

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
+
 from setuptools import setup
 
 from clippy import __author__ as APP_AUTHOR
 from clippy import __contact__ as APP_CONTACT
 from clippy import __version__ as APP_VERSION
+
+if env_file := os.getenv("GITHUB_ENV"):
+    print(f"Github environment detected! Setting env.VERSION_TAG to v{APP_VERSION}")
+    with open(env_file, "a") as f:
+        f.write(f"VERSION_TAG=v{APP_VERSION}\n")
 
 APP = ["clippy.py"]
 OPTIONS = {
