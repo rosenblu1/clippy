d9a3ff9f39dc4ff1d9d93dbf18efee04656b4654
rosenblu1 <yaplore@gmail.com>
2023-05-17 14:18:58 -0400

    [NOBUILD] trying to write vers tag to file

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ from clippy import __author__ as APP_AUTHOR
 from clippy import __contact__ as APP_CONTACT
 from clippy import __version__ as APP_VERSION
 
-if env_file := os.getenv("GITHUB_ENV"):
-    print(f"Github environment detected! Setting env.VERSION_TAG to v{APP_VERSION}")
-    with open(env_file, "a") as f:
-        f.write(f"VERSION_TAG=v{APP_VERSION}\n")
+VERS_FILE = "vers.txt"
+
+print(f"Writing to version file ({VERS_FILE}): v{APP_VERSION}")
+with open(VERS_FILE, "w") as f:
+    f.write(f"v{APP_VERSION}")
 
 APP = ["clippy.py"]
 OPTIONS = {
